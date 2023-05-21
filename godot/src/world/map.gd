06541a6425f4da90d394de39b9ac16b080fe31dc
erebrus <erebrus@gmail.com>
2023-05-21 19:39:01 +0200

    integrated bug version1

--- godot/src/world/map.gd
+++ godot/src/world/map.gd
@@ -8,17 +8,17 @@ var dimension = Events.Dimension.SPECTRAL
 func _ready():
 	Events.dimension_changed.emit(dimension)
 	Events.dimension_changed.connect(_on_dimension_changed)
-
+	
 
 func _on_dimension_changed(_dimension):	
 	if dimension == _dimension:
 		return
 	dimension = _dimension
 	spectral_canvas.visible = dimension == Events.Dimension.SPECTRAL
-	if dimension == Events.Dimension.MATERIAL:
-		$TileMap.tile_set = SPECTRAL_TS
-	else:
-		$TileMap.tile_set = MATERIAL_TS
+#	if dimension == Events.Dimension.MATERIAL:
+#		$TileMap.tile_set = SPECTRAL_TS
+#	else:
+#		$TileMap.tile_set = MATERIAL_TS
 #	if dimension == Events.Dimension.MATERIAL:
 #		$AnimationPlayer.play("to_material")
 #	else:
