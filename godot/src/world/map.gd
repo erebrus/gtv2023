f5be1241debe8cbf60bdd1614348fdebcc34631d
Emily <tweetychrip@gmail.com>
2023-05-21 14:05:20 -0700

    applied shaders to background

--- godot/src/world/map.gd
+++ godot/src/world/map.gd
@@ -3,7 +3,7 @@ extends Node2D
 const MATERIAL_TS = preload("res://src/world/cemetery_tileset_material.tres")
 const SPECTRAL_TS = preload("res://src/world/cemetery_tileset_spectral.tres")
 var dimension = Events.Dimension.SPECTRAL
-@onready var spectral_canvas = $spectral_canvas
+@onready var spectral_canvas = $ParallaxBackground/SpectralCanvas
 
 func _ready():
 	Events.dimension_changed.emit(dimension)
