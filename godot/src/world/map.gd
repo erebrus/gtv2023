c51eb3f127b888ba59012a853a8b984a67a6b152
erebrus <erebrus@gmail.com>
2023-05-22 06:00:38 +0200

    fixed fog and tint

--- godot/src/world/map.gd
+++ godot/src/world/map.gd
@@ -16,9 +16,14 @@ func _on_dimension_changed(_dimension):
 	dimension = _dimension
 	spectral_canvas.visible = dimension == Events.Dimension.SPECTRAL
 	if dimension == Events.Dimension.MATERIAL:
-		$TileMap.tile_set = SPECTRAL_TS
-	else:
 		$TileMap.tile_set = MATERIAL_TS
+		$Foreground/ParallaxLayer/Fog.visible = false
+		$CanvasLayer/Tint.visible = false
+	else:
+		$TileMap.tile_set = SPECTRAL_TS
+		$Foreground/ParallaxLayer/Fog.visible = true
+		$CanvasLayer/Tint.visible = true
+	
 #	if dimension == Events.Dimension.MATERIAL:
 #		$AnimationPlayer.play("to_material")
 #	else:
