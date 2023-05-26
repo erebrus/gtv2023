173ab2752ab77e80f2eeaf8efec775d7337abbc4
erebrus <erebrus@gmail.com>
2023-05-26 09:48:22 +0200

    lever complete

--- godot/src/world/objects/lever.gd
+++ godot/src/world/objects/lever.gd
@@ -1,19 +1,27 @@
+@tool
 extends Area2D
 
 signal switched(value:bool)
 
 @export var value := false
+
 var player
 
 func _ready():
 	if value:
-		switch()
+		interact()
 	Events.dimension_changed.connect(_on_dimension_changed)		
 
 
-func switch():
+func interact():
+	
 	value = not value
+	Logger.info("%s switched . new value=%s" % [name, value])
 	switched.emit(value)
+	if value:
+		$sprite.play("on")
+	else:
+		$sprite.play("off")
 		
 func _on_dimension_changed(dimension):	
 	if dimension == Events.Dimension.SPECTRAL and player:
@@ -24,11 +32,14 @@ func _on_dimension_changed(dimension):
 
 func _on_body_entered(body):
 	if body.has_method("set_object"):
+		Logger.info("%s player detected " % name)
 		body.set_object(self)
 		player = body
 
 
 func _on_body_exited(body):
+	Logger.info("%s player lost " % name)
 	if body.has_method("unset_object"):
 		body.unset_object(self)
-		player = null
+		
+	player = null
