933f513d1224b03b4911398186d1e8ea515febd0
erebrus <erebrus@gmail.com>
2023-05-27 06:02:19 +0200

    dash spamming

--- godot/src/player/platform_controller.gd
+++ godot/src/player/platform_controller.gd
@@ -146,14 +146,16 @@ func control(_delta:float) -> void:
 
 	
 func do_dash(direction:Vector2 = Vector2.ZERO)->void:
-	Logger.trace("dash")
+	Logger.debug("dash at %d" % Time.get_ticks_msec())
 	can_dash=false
 	#no direction provided, use facing direction
 	if direction == Vector2.ZERO:
-#		player.velocity.x=dash_boost*player.last_direction.x
-		player.velocity=dash_boost*Vector2.UP
-	else:
-		player.velocity=dash_boost*direction
+		direction = Vector2.UP
+	if not player.is_on_floor():
+		direction.y = clamp(direction.y,-.4,.4)
+		Logger.info ("dash speed trimmed.")
+	player.velocity=dash_boost*direction
+		
 	Logger.info("dash speed %s" % player.velocity)
 	player.on_dash()
 
