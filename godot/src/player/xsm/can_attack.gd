0541760272e6716265063118b825827793242c53
erebrus <erebrus@gmail.com>
2023-05-27 06:22:36 +0200

    preventing attack within attack

--- godot/src/player/xsm/can_attack.gd
+++ godot/src/player/xsm/can_attack.gd
@@ -1,5 +1,5 @@
 extends State
 
 func _on_update(_delta: float) -> void:
-	if Input.is_action_just_pressed("attack"):
+	if not is_active("attack") and Input.is_action_just_pressed("attack"):
 		change_state("attack")
