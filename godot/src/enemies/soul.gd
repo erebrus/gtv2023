e7ed0e3805e03a487b3ffc6056a8aa03eda87b9b
erebrus <erebrus@gmail.com>
2023-05-29 13:30:20 +0200

    soul anchors

--- godot/src/enemies/soul.gd
+++ godot/src/enemies/soul.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 signal died
+enum State {Flying, Hovering}
  
 @export var energy:=20.0
 @onready var sfx := $soul/sfx_consume
@@ -15,16 +16,17 @@ var direction:float = 1.0 if RngUtils.int_range(0,1)>0.0 else -1.0
 	set(_anchor):
 		anchor = _anchor
 		global_position = anchor
-@export var travel_distance := Vector2(1920, 1080.0)
-@export var travel_speed := 100.0
+@export var travel_distance := 1920.0
+@export var travel_speed := 300.0
 var target: Vector2
-@onready var no_amp_pos := global_position
-
+var state := State.Hovering
+var current_anchor
 
 func _ready():
 	Events.dimension_changed.connect(_on_dimension_changed)
 	fade_in(false)
-	set_target()
+	_on_timer_timeout()
+		
 
 func _on_dimension_changed(_dimension):
 	if _dimension == Events.Dimension.MATERIAL:
@@ -34,18 +36,47 @@ func _on_dimension_changed(_dimension):
 		visible = true
 		$soul.monitoring = true
 		
-func set_target() -> void:
-	target = Vector2(RngUtils.float_range(anchor.x - travel_distance.x / 2, anchor.x + travel_distance.x / 2), RngUtils.float_range(anchor.y - travel_distance.y / 2, anchor.y + travel_distance.y / 2))
+func set_target() -> bool:
+	var targets:Array = []
+	target = global_position
+	for t in get_tree().get_nodes_in_group("soul_anchor"):
+		var dist = abs(global_position.x - t.global_position.x)
+		if dist<travel_distance\
+			and dist > 78\
+			and t.soul == null: 				
+				targets.append(t)
+	if not targets.is_empty():
+		var t = RngUtils.array(targets, 1)[0]
+		target = t.global_position + Vector2(RngUtils.float_range(-64,64),RngUtils.float_range(-64,64))
+		Logger.debug("%s choosing target %s" % [name, target])
+		if current_anchor:
+			current_anchor.soul = null
+		current_anchor=t
+		current_anchor.soul= self
+		return true
+	else:
+		Logger.debug("% no soul anchor found. Trying again later" % name)
+		$Timer.start()
+		return false
+		
 	
 	
 func _process(delta):
-	if no_amp_pos.distance_to(target) <= travel_speed * delta:
-		set_target()
-	var period = Time.get_ticks_msec()/200.0+seed_delta
-	no_amp_pos += no_amp_pos.direction_to(target) * delta * travel_speed
-	var target_pos := no_amp_pos + Vector2(0, sin(period)* y_amplitude)
-	velocity = position.direction_to(target_pos) * travel_speed
-	move_and_slide()
+	if state == State.Flying:
+		if global_position.distance_to(target) <= travel_speed * delta:
+			anchor=global_position
+			state=State.Hovering
+			$Timer.wait_time = RngUtils.float_range(5,10)
+			$Timer.start()
+			
+		velocity = global_position.direction_to(target) * travel_speed
+		var tv = velocity
+		var gp = global_position
+		move_and_slide()		
+	else:
+		var period = Time.get_ticks_msec()/200.0+seed_delta
+		position.y = anchor.y + sin(period)* y_amplitude
+		
 #	x_speed = sin(period)* .5 * direction
 #	position.x += x_speed
 
@@ -68,3 +99,8 @@ func _on_soul_body_entered(body: Node2D) -> void:
 	died.emit()
 	call_deferred("queue_free")
 
+
+
+func _on_timer_timeout():
+	if set_target():
+		state = State.Flying
